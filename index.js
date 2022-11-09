04d15dc9fd3204c2508496afa3f78f4b632144d6
Farhan Hoque <farhan@Farhans-MacBook-Pro.local>
2022-11-09 15:28:46 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sq1hraq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 app.get('/', (req, res) => {
     res.send('server running')
 })
