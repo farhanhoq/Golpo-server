20b2cd240f90e376fcccb0e325ee0ba8bca69669
Farhan Hoque <farhan@Farhans-MacBook-Pro.local>
2022-11-09 16:09:47 +0600

    get service data from db

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sq1hraq.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db('golpo').collection('services');
+        
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+        
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('server running')
 })
+run().catch((err) => console.error(err));
 
 app.listen(port, () => {
     console.log(port)
